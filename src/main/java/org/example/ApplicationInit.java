2f95778d66fddea091cc21f0080525e40d6d0881
PrethamMuthappa <prethambachira1819@gmail.com>
2024-04-23 01:21:30 +0530

    changes 2

--- src/main/java/org/example/ApplicationInit.java
+++ src/main/java/org/example/ApplicationInit.java
@@ -3,25 +3,27 @@ package org.example;
 import net.miginfocom.swing.MigLayout;
 
 import javax.swing.*;
+import java.awt.*;
 
- class ApplicationInit extends JFrame {
+class ApplicationInit extends JFrame {
 
      static void runs(){
-         JFrame frame = new JFrame("My Application");
+         JFrame frame = new JFrame();
          frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
          JPanel panel = new JPanel(new MigLayout());
+         JLabel title = new JLabel("ONE TAP");
+         JLabel description=new JLabel("  A simple tool for clearing your systems cache/temporary files or trash files");
+         JSeparator separator=new JSeparator();
          frame.add(panel);
          frame.setResizable(false);
-         JFileChooser fileChooser = new JFileChooser();
-         JButton button = new JButton("Click Me!");
-         panel.add(button,fileChooser);
-
+         panel.add(title,"center,wrap");
+         panel.add(separator,"center,growx,wrap");
+         panel.add(description,"center,wrap");
          frame.pack();
-         frame.setSize(600,800);
+         frame.setSize(500,500);
          frame.setVisible(true);
 
      }
-
 
 }
--- src/main/java/org/example/MainFrame.java
+++ src/main/java/org/example/MainFrame.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import com.formdev.flatlaf.FlatLaf;
+import com.formdev.flatlaf.fonts.roboto.FlatRobotoFont;
 import com.formdev.flatlaf.themes.FlatMacDarkLaf;
 
 import javax.swing.*;
@@ -8,8 +9,10 @@ import java.awt.*;
 public class MainFrame {
     public static void main(String[] args) {
         FlatMacDarkLaf.setup();
+        FlatRobotoFont.install();
         UIManager.put("Button.background",new Color(173,216,230));
         UIManager.put("Button.foreground",new Color(1,1,1));
+        UIManager.put("default",new Font(FlatRobotoFont.FAMILY,Font.PLAIN,13));
         ApplicationInit.runs();
     }
 }
